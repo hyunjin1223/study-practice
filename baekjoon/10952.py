09c8c661824b9326f86e80d895c90aaaf4693034
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 23:08:26 +0900

    sum until zero

--- baekjoon/10952.py
+++ baekjoon/10952.py
@@ -2,10 +2,12 @@
 # tier: bronze
 
 while True:
-    
+    # 두 정수 입력
     A, B = map(int, input().split())
     
+    # 0 0 입력 시 종료
     if A == 0 and B == 0:
         break
     
+    # 두 수의 합 출력
     print(A + B)
