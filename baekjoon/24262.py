6569c2672b0e5b36c3db1786b5a6f6797aa62d3c
Hyunjin Baek <hyunjin3399@gmail.com>
2026-05-05 23:35:52 +0900

    time complexity 1

---
+++ baekjoon/24262.py
@@ -0,0 +1,12 @@
+# problem: 24262
+# tier: bronze
+
+# MenOfPassion(A, n) {
+#     i = ⌊n / 2⌋;
+#     return A[i]; # 코드1
+# }
+
+# 입력받은 n에 상관없이 코드1은 딱 한 번만 실행됨
+print(1)
+# n의 차수는 0 (상수 시간 복잡도 O(1))
+print(0)
